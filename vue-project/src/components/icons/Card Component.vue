e02a530400d53c4e701b287e3719088042b804c8
MyHappySun <112709363+DahakaDevelopDevOps@users.noreply.github.com>
2023-03-01 18:48:18 +0300

    commiting lab 5 (only components)

---
+++ vue-project/src/components/icons/Card Component.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card">
+    <img :src="image" alt="Card Image" class="card__image">
+    <div class="card__content">
+      <h3 class="card__title">{{ title }}</h3>
+      <p class="card__description">{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    image: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+/* add styles for the card component */
+</style>
---
+++ vue-project/src/components/icons/object.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="hero" :style="{ backgroundImage: `url(${backgroundImage})` }">
+    <div class="hero__content">
+      <h1 class="hero__title">{{ title }}</h1>
+      <p class="hero__subtitle">{{ subtitle }}</p>
+      <a class="hero__button" :href="ctaButton.url">{{ ctaButton.label }}</a>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    subtitle: {
+      type: String,
+      required: true
+    },
+    backgroundImage: {
+      type: String,
+      required: true
+    },
+    ctaButton: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+/* add styles for the hero component */
+</style>
