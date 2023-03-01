b0277a20f0a5f1552f820c9bd3c90530bcb43304
MyHappySun <112709363+DahakaDevelopDevOps@users.noreply.github.com>
2023-03-01 18:50:05 +0300

    adding propses

---
+++ vue-project/src/components/icons/Navigation.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navigation">
+    <div class="logo">
+      <img :src="logo" alt="Logo" />
+    </div>
+    <ul class="navigation__list">
+      <li class="navigation__item" v-for="(link, index) in links" :key="index">
+        <a :href="link.url" :class="{ 'active': activeLink === link.label }">{{ link.label }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  props: {
+    logo: {
+      type: String,
+      required: true
+    },
+    links: {
+      type: Array,
+      required: true
+    },
+    activeLink: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+/* add styles for the navigation component */
+</style>
---
+++ vue-project/src/components/icons/Testimonial component.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="testimonial">
+    <div class="testimonial__avatar">
+      <img :src="avatar" alt="Testimonial Author Avatar" />
+    </div>
+    <div class="testimonial__content">
+      <p class="testimonial__text">{{ content }}</p>
+      <p class="testimonial__author">{{ author }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Testimonial',
+  props: {
+    avatar: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+/*
